f914dcd80e0833a1db453681efa1221d4c438c05
Matthew Kurniawan <mdkcode88@gmail.com>
2022-07-01 23:42:30 -0400

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,40 @@
 export default function Index() {
     return (
-        <>
-            <h1>Hello world!</h1>
-            <p>This is a starter template you can use for Web Dev for Makers projects.</p>
-            <img src="/logo.png" alt="Web Dev for Makers Logo" />
+        <>  
+        <head>
+            <title>Matthew Kurniawan</title>
+            <meta name="description" content="Welcome to my portfolio"/>
+        </head>
+            <div id="Navbar">
+                <span>Matthew Kurniawan</span>
+            </div>
+            <div id="container"> 
+                <div id="top-section"> 
+                    <img src="/profile.jpg" id="prof-pic" alt="profile"/>
+                    <h1>Hey, nice to see you around!<br/> I'm Matthew</h1>
+                </div>
+                <p></p>
+                <p></p>
+                <p></p>
+                <hr/>
+                <p>Some projects I've built</p>
+                <a href="" className="proj-wrapper">
+                <div className="project-box">
+                    <img src=""/>
+                    <h2>Web3 Messaging Platform</h2>
+                    <p>Send GIFs and crypto to friends</p>
+                
+                </div>
+                </a>
+                <a href="" className="proj-wrapper">
+                <div className="project-box">
+                    <img src=""/>
+                    <h2>ASOSHI</h2>
+                    <p>Gen-Z Networking Platform to connect with driven founders</p>
+                
+                </div>
+                </a>
+            </div>
         </>
     );
 }
